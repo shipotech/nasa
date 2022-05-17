e9da121a555709f3874668e95b73843d4c815120
Mario Montano <shipotech@gmail.com>
2022-05-16 20:35:42 -0500

    Add CORS package on our Server and manage CORS to whitelist our Client server

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
-app.use(express.json());
 
 // Routes
 // Planets
 const planetsRouter = require('./routes/planets/planets.router');
 const planetsController = require('./routes/planets/planets.controller');
+
+// Middlewares
+// CORS has an object with a list of whitelisted origins
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+app.use(express.json());
 app.use(planetsRouter);
 
 module.exports = app;
