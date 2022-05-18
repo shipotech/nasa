1a0f77eb6e2682a248808ae0e0ef32e3fec266a4
Mario Montano <shipotech@gmail.com>
2022-05-17 22:31:06 -0500

    Add logging with morgan package

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require("path");
 const cors = require('cors');
+const morgan = require("morgan");
 
 const app = express();
 
@@ -14,8 +15,13 @@ const planetsController = require('./routes/planets/planets.controller');
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+// Logging
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public'))); // serve static files built from Client
+
+// Routes
 app.use(planetsRouter);
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
