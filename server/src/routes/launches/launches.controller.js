e84a8c61604c2688faa7ea6c07e5a8aea500a3dc
Mario Montano <shipotech@gmail.com>
2022-05-20 22:28:25 -0500

    Add basic validations to launches controller

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -11,11 +11,26 @@ function httpAddNewLaunch(req, res) {
     // Get the new launch from the request body
     const launch = req.body;
 
+    // Making validations
+    if (!launch.mission || !launch.rocket || !launch.launchDate || !launch.destination) {
+        return res.status(400).json({
+            error: 'Missing required launch fields'
+        });
+    }
+
     // Convert date to date object
     launch.launchDate = new Date(launch.launchDate);
+
+    // Add date validation
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date'
+        });
+    }
+
     addNewLaunch(launch);
 
-    return res.status(201).json(req.body);
+    return res.status(201).json(launch);
 }
 
 module.exports = {
