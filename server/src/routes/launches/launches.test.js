5cb4308ced7348ba96a3fde601575975d9c61933
Mario Montano <shipotech@gmail.com>
2022-05-22 12:16:48 -0500

    Add SuperTest package (run requests to our server)

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -1,17 +1,45 @@
+const request = require('supertest');
+const app = require('../../app');
+
 describe('Test GET /launches', () => {
-  test('It should respond with 200 success',() => {
-    let response = 200
-    expect(response).toBe(200);
+  test('It should respond with 200 success',async () => {
+    const response = await request(app)
+      .get('/launches')
+      .expect('Content-Type', /json/)
+      .expect(200)
   })
 });
 
 
 describe('Test POST /launches', () => {
-  test('It should respond with 201 success',() => {
-    let response = 201
-    expect(response).toBe(201);
+
+  const completeLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'Falcon 9',
+    target: 'Kepler-22 b',
+    launchDate: 'January 4, 2028'
+  };
+
+  const incompleteLaunchData = {
+    mission: 'USS Enterprise',
+    rocket: 'Falcon 9',
+    target: 'Kepler-22 b'
+  };
+
+  test('It should respond with 201 created',async () => {
+    const response = await request(app)
+      .post('/launches')
+      .send(completeLaunchData)
+      .expect('Content-Type', /json/)
+      .expect(201)
+
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(incompleteLaunchData);
   })
 
-  test('It should catch missing required properties',() => {})
   test('It should catch invalid dates',() => {})
+  test('It should catch missing required properties',() => {})
 });
