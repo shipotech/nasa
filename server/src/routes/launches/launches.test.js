85729b80c4ff8c43fc2b74526df1f15dbb1efe03
Mario Montano <shipotech@gmail.com>
2022-06-05 17:18:05 -0500

    Add versioning to the test launches file

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -29,7 +29,7 @@ describe('Launches API', () => {
   describe('Test GET /launches', () => {
     test('It should respond with 200 success',async () => {
       const response = await request(app)
-        .get('/launches')
+        .get('/v1/launches')
         .expect('Content-Type', /json/)
         .expect(200)
     })
@@ -60,7 +60,7 @@ describe('Launches API', () => {
 
     test('It should respond with 201 created',async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(completeLaunchData)
         .expect('Content-Type', /json/)
         .expect(201)
@@ -74,7 +74,7 @@ describe('Launches API', () => {
 
     test('It should catch missing required properties',async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(incompleteLaunchData)
         .expect('Content-Type', /json/)
         .expect(400)
@@ -86,7 +86,7 @@ describe('Launches API', () => {
 
     test('It should catch invalid dates',async () => {
       const response = await request(app)
-        .post('/launches')
+        .post('/v1/launches')
         .send(invalidLaunchDate)
         .expect('Content-Type', /json/)
         .expect(400)
